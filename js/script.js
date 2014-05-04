c4d304a797c17f8cf90c31682d18b18c525e454a
usain <dylanscott87@gmail.com>
2014-05-04 16:43:03 +1200

    Mobile home page, some interaction.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
-/* Author: 
-
+/* 	Author: Dylan Scott 
+	Date: May 2014
 */
+$(document).ready(function(){
+
+	var $mailchimpDraw 	= $("#mc_embed_signup"),
+		$emailButton 	= $(".icon-envelope"),
+		$close			= $(".close"),
+		$yearClick		= $(".click"),
+		$exhibition		= $(".year");
+
+	$mailchimpDraw.hide();
+	$exhibition.hide();
+
+	$emailButton.click(function(e){
+
+		e.preventDefault();
+		$mailchimpDraw.slideToggle(400);
+
+	});
+
+	$close.click(function(e){
+
+		e.preventDefault();
+		$mailchimpDraw.slideToggle(400);
+
+	});
+
+	$yearClick.click(function(e){
+
+		e.preventDefault();
+		$(this).next().slideToggle(400);
+
+	});
+
+
+
+});//end of ready
 
 
 
