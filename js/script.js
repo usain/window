b2eb3d6c07c23aafe87c6567472a4beb8c67f46e
Dylan Scott <dylanscott@Dylans-MacBook-Air.local>
2014-05-12 22:29:16 +1200

    redefined font styles as 'em's

--- js/script.js
+++ js/script.js
@@ -21,6 +21,20 @@ $(document).ready(function(){
 	$navItems.hide();
 	//$exhibition.hide();
 
+//~~~~~~~~~~~~~~~~~ placeholder ~~~~~~~~~~~~~~~~~//
+
+	$('input, textarea').placeholder();
+
+	var html;
+				if ($.fn.placeholder.input && $.fn.placeholder.textarea) {
+					html = '<strong>Your current browser natively supports <code>placeholder</code> for <code>input</code> and <code>textarea</code> elements.</strong> The plugin won’t run in this case, since it’s not needed. If you want to test the plugin, use an older browser ;)';
+				} else if ($.fn.placeholder.input) {
+					html = '<strong>Your current browser natively supports <code>placeholder</code> for <code>input</code> elements, but not for <code>textarea</code> elements.</strong> The plugin will only do its thang on the <code>textarea</code>s.';
+				}
+				if (html) {
+					$('<p class="note">' + html + '</p>').insertAfter('form');
+				}
+
 //mailchimp draw
 
 	$emailButton.click(function(e){
@@ -93,6 +107,5 @@ $(document).ready(function(){
 
 		$('.showText').slideToggle(400);
 	});
-
 
 });//end of ready
